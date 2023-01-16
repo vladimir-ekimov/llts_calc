d7b1f5f6987860d200d699e04dc71833937e200a
Vladimir Ekimov <avenger@eskape2play.com>
2023-01-16 13:58:47 +0200

    Moved types in a common package. Published to npm. client and server now both use llnkts-common:1.0.4

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,12 +1,3 @@
-rm -rf node_modules
-rm -rf dist
-cd ../server
-rm -rf node_modules
-rm -rf dist
-cd ../client
-rm -rf node_modules
-rm -rf dist
-cd ../scripts
 docker system prune -a -f
 docker image prune -a -f
 docker volume prune -f
